bfbef4a2f610c69be8e82d24008d265566fb512e
ETweb51 <webdev.et@gmail.com>
2021-07-25 18:59:58 +0200

    Update scripts.js
    Task 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
 let pokemonList = [
   {
-    name = 'Bulbasaur',
-    type = ['grass', 'poison'],
-    height = 7
+    name : 'Bulbasaur',
+    type : ['grass', 'poison'],
+    height : 0.7
   },
 
   {
-    name = 'Charmander',
-    type = 'fire',
-    height = 6
+    name : 'Charmander',
+    type : 'fire',
+    height : 0.6
   },
 
   {
-    name = 'Squirtle',
-    type = 'water',
-    height = 5
+    name : 'Squirtle',
+    type : 'water',
+    height : 0.5
   }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height < 0.6) {
+    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) - I am a small Pokémon</p>`);
+  } else {
+    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height})</p>`);
+  }
+}
