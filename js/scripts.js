62d0296f1245b863a7d14d5f822dd969944ffb67
ETweb51 <webdev.et@gmail.com>
2021-07-25 14:29:14 +0200

    Change of script.js
    pokemonList added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-alert('Hello World');
-let favoriteFood = 'Kulak hamuru';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name = 'Bulbasaur',
+    type = ['grass', 'poison'],
+    height = 7
+  },
+
+  {
+    name = 'Charmander',
+    type = 'fire',
+    height = 6
+  },
+
+  {
+    name = 'Squirtle',
+    type = 'water',
+    height = 5
+  }
+];
