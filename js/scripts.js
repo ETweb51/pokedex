95a6d67b0040473c4df6b7f9912fe0f924d57a23
ETweb51 <webdev.et@gmail.com>
2021-07-27 13:19:29 +0200

    Task-1.5.
    Second part and also bonus tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,51 @@
-let pokemonList = [
-  {
-    name : 'Bulbasaur',
-    type : ['grass', 'poison'],
-    height : 0.7
-  },
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {
+      name : 'Bulbasaur',
+      type : ['grass', 'poison'],
+      height : 0.7
+    },
 
-  {
-    name : 'Charmander',
-    type : 'fire',
-    height : 0.6
-  },
+    {
+      name : 'Charmander',
+      type : ['fire'],
+      height : 0.6
+    },
 
-  {
-    name : 'Squirtle',
-    type : 'water',
-    height : 0.5
+    {
+      name : 'Squirtle',
+      type : ['water'],
+      height : 0.5
+    }
+  ];
+
+  function getAll() {
+    return pokemonList;
   }
-];
 
-pokemonList.forEach(function(pokemon) {
+  function add(pokemon) {
+    if (typeof pokemon === 'object' && Object.keys(pokemon) === 'name', 'type', 'height') {
+      pokemonList.push(pokemon);
+    }
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+}) ();
+
+// Adding an item, which fullfill the criteria
+pokemonRepository.add({name: 'Pikachu', type: ['electric'], height: 0.4});
+
+// Adding an item, which does not fulfill the criterie of beeing an object
+pokemonRepository.add('name: Pikachu, type: ["electric"], height: 0.4');
+
+// Adding an item, which fullfill the criteria of beeing an object but not fullfilling the keys criteria
+pokemonRepository.add({country: 'Germany', city: 'Berlin', type: 'capital city'});
+
+// Prints the list with Pokémons, which are returned by the getAll()-function
+pokemonRepository.getAll().forEach(function(pokemon) {
   if (pokemon.type.includes('poison')) {
     document.write(`<p>${pokemon.name} (height: ${pokemon.height}) - Beware I am a poisonous Pokémon</p>`);
   } else {
